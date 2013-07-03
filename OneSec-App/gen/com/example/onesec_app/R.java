020b3a13b2f5c9156a476790d5243ac7cc829335
Vanessa Ronan <vmronan@gmail.com>
2013-07-03 12:04:53 -0700

    Added some camera functionality to mainactivity?

--- OneSec-App/gen/com/example/onesec_app/R.java
+++ OneSec-App/gen/com/example/onesec_app/R.java
@@ -24,18 +24,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int takeSecond=0x7f080000;
+        public static final int viewCakes=0x7f080002;
+        public static final int viewSeconds=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_new_cake=0x7f030001;
+        public static final int activity_new_second=0x7f030002;
+        public static final int activity_recipe=0x7f030003;
+        public static final int activity_view_cakes=0x7f030004;
+        public static final int activity_view_seconds=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int new_cake=0x7f070001;
+        public static final int new_second=0x7f070002;
+        public static final int recipe=0x7f070003;
+        public static final int view_cakes=0x7f070004;
+        public static final int view_seconds=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int text_takeSecond=0x7f050008;
+        public static final int text_viewCakes=0x7f05000a;
+        public static final int text_viewSeconds=0x7f050009;
+        public static final int title_activity_new_cake=0x7f050004;
+        public static final int title_activity_new_second=0x7f050003;
+        public static final int title_activity_recipe=0x7f050007;
+        public static final int title_activity_view_cakes=0x7f050006;
+        public static final int title_activity_view_seconds=0x7f050005;
     }
     public static final class style {
         /** 
