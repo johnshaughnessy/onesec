6104ebcb82da34d6bc4c545c532e1fa1e592eb98
John Shaughnessy <johnfshaughnessy@gmail.com>
2013-07-10 19:53:18 -0700

    dat master branch #yolo

--- OneSec-Android/src/com/example/onesec/impl/http/TokenManager.java
+++ OneSec-Android/src/com/example/onesec/impl/http/TokenManager.java
@@ -16,11 +16,6 @@ public class TokenManager {
 	public static void generateAndSaveToken(final Context ctx, String email, String password){
 		RequestParams params = OneSecRestClient.buildParams(new String[] {"email", "password"}, new String[] {email, password});
 		OneSecRestClient.post("tokens.json", params, new AsyncHttpResponseHandler(){
-			@Override
-    		public void onStart() {
-    			Log.v("generate token", "onStart()");
-    			super.onStart();
-    		}
     		@Override
     		public void onSuccess(String response) {
     			Log.v("generate token", "onSuccess");
@@ -34,21 +29,6 @@ public class TokenManager {
 				}
     			Log.v("generate token", response);
     			saveToken(ctx, token);
-    		}
-    		
-    		@Override
-    		public void onFailure(Throwable e, String response) {
-    			// Response failed :(
-    			Log.v("generate token", "onFailure() has the response: " + response);
-    			e.printStackTrace();
-    			super.onFailure(e, response);
-    		}
-    		
-    		@Override
-    		public void onFinish() {
-    			// Completed the request (either success or failure)
-    			Log.v("generate token", "onFinish()");
-    			super.onFinish();
     		}
     	});
 	}
